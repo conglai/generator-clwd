6a37baeaf0b17186502cdac06eda6f38119929b4
ykan_ys <ykan_ys@163.com>
2016-09-13 09:58:38 +0800

    update rc gulpfile

--- generators/rc/templates/files/gulpfile.js
+++ generators/rc/templates/files/gulpfile.js
@@ -14,12 +14,22 @@ const browserSync = require('browser-sync').create();
 gulp.task('web', function() {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: './',
+      middleware: function (req, res, next) {
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        next();
+      }
+
     }
   });
 });
 
 const webpack = require('webpack');
+let unf;
+let typeMap = {
+  'less': 'css'
+}
 gulp.task('default', ['prepare', 'web'], () => {
   webpack(webpackConfig).watch({}, (err, stats) => {
     gutil.log(stats.toString({
@@ -29,7 +39,17 @@ gulp.task('default', ['prepare', 'web'], () => {
     }));
   });
 
-  gulp.watch(['build/*.js', 'build/*.css'], path => {
-    browserSync.reload(path);
+  let reloadType;
+  gulp.watch('src/*.+(js|less|jsx)', file => {
+    let fileType = file.path.split('.').pop();
+    reloadType = typeMap[fileType] || fileType;
+
+    gutil.log(gutil.colors.green(`${reloadType} has changed....`));
+  });
+
+  gulp.watch('build/*.+(js|css)', file => {
+    if(!reloadType) return;
+    browserSync.reload(`*.${reloadType}`);
+    reloadType = unf;
   });
 });
